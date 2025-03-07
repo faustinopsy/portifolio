f5cf3bbc9c137179d6edf48d78cd2cedf82fddf5
faustinopsy <rodrigohipnose@gmail.com>
2025-03-07 15:15:57 -0300

    add nav molecula

---
+++ src/components/molecules/NavList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from '../atoms/Nav';
+import UnorderedList from '../atoms/UnorderedList';
+import ListItem from '../atoms/ListItem';
+import Anchor from '../atoms/Anchor';
+
+export default function NavList() {
+  const listStyles = {
+    display: 'flex',
+    gap: '16px',
+    listStyle: 'none',
+  };
+
+  return (
+    <Nav>
+      <UnorderedList style={listStyles}>
+        <ListItem>
+          <Anchor href="#home">Home</Anchor>
+        </ListItem>
+        <ListItem>
+          <Anchor href="#servicos">Serviços</Anchor>
+        </ListItem>
+        <ListItem>
+          <Anchor href="#portfolio">Portfólio</Anchor>
+        </ListItem>
+        <ListItem>
+          <Anchor href="#contato">Contato</Anchor>
+        </ListItem>
+      </UnorderedList>
+    </Nav>
+  );
+}
